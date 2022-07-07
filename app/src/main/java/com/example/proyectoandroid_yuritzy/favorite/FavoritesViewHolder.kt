a80aa58fa407c3029197b972767bc1a0b989fd5f
Braulio Jimenez <bjimenez@microsoft.com>
2022-07-06 23:02:50 -0500

    favorites fragment was updated.

--- app/src/main/java/com/example/proyectoandroid_yuritzy/favorite/FavoritesViewHolder.kt
+++ app/src/main/java/com/example/proyectoandroid_yuritzy/favorite/FavoritesViewHolder.kt
@@ -1,7 +1,10 @@
 package com.example.proyectoandroid_yuritzy.favorite
 
 import android.view.View
+import android.widget.ImageView
+import android.widget.RatingBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.proyectoandroid_yuritzy.R
 import com.example.proyectoandroid_yuritzy.main.Product
@@ -10,6 +13,10 @@ class FavoritesViewHolder(private val view: View): RecyclerView.ViewHolder(view)
 
     fun setItem(product: Product) {
         view.findViewById<TextView>(R.id.tv_thematic).text = product.name
+        view.findViewById<ImageView>(R.id.imageView2).setImageDrawable(ContextCompat.getDrawable(view.context, product.image?: 0))
+        view.findViewById<RatingBar>(R.id.ratingBar2).rating = product.rating?.toFloat()?: 0F
+        view.findViewById<TextView>(R.id.tv_final_price).text = view.context.getString(R.string.price_final, product.priceWithDiscount)
+
     }
 
 }
